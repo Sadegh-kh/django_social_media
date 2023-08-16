7f84c702baf7d05a2fcefa1419668af7a43a41dd
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-08-16 15:22:01 +0330

    init cutom user on model and admin

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ["username", "phone", "first_name", "last_name", "email", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional Information",
+            {
+                "fields": ("photo", "job", "age", "birth_date", "bio"),
+            },
+        ),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional Information",
+            {
+                "fields": ("photo", "job", "age", "birth_date", "bio"),
+            },
+        ),
+    )
