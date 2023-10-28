df8c1e87839fad631ab225fb532fd6f6331b8f2b
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-10-29 00:38:35 +0330

    init follow and following relation model

--- accounts/models.py
+++ accounts/models.py
@@ -15,3 +15,26 @@ class User(AbstractUser):
     bio = models.TextField(null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     photo = models.ImageField(upload_to=user_dirctory_path, null=True, blank=True)
+
+    followers = models.ManyToManyField(
+        "self",
+        through="Contact",
+        related_name="following",
+        symmetrical=False,
+    )
+
+
+class Contact(models.Model):
+    user_from = models.ForeignKey(
+        User, related_name="rel_from_set", on_delete=models.CASCADE
+    )
+    user_to = models.ForeignKey(
+        User, related_name="rel_to_set", on_delete=models.CASCADE
+    )
+    follow_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-follow_at"]
+        indexes = [
+            models.Index(fields=["-follow_at"]),
+        ]
